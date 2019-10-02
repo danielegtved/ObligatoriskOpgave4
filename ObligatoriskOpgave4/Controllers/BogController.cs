19a798d52abb4b537ccf11fe027d70ca86043226
danielegtved <43374944+danielegtved@users.noreply.github.com>
2019-10-02 11:14:58 +0200

    REST Done

--- ObligatoriskOpgave4/Controllers/BogController.cs
+++ ObligatoriskOpgave4/Controllers/BogController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ObligatoriskOpgave1;
 
 namespace ObligatoriskOpgave4.Controllers
 {
@@ -11,44 +12,55 @@ namespace ObligatoriskOpgave4.Controllers
     [ApiController]
     public class BogController : ControllerBase
     {
-        //public static List<Bog> BogList = new List<Bog>()
-        //{
-        //    new Bog("Very Poter", "Gertrud", 12, "1234657980123"),
-        //    new Bog("Little Poter", "Gertrud", 13, "1234657980124"),
-        //    new Bog("Big Poter", "Gertrud", 666, "1234657980125"),
-        //    new Bog("Poter", "Gertrud", 12, "1234657980126")
-        //};
+        public static List<Bog> BogList = new List<Bog>()
+        {
+            new Bog("Very Poter", "Gertrud", 12, "1234657980123"),
+            new Bog("Little Poter", "Gertrud", 13, "1234657980124"),
+            new Bog("Big Poter", "Gertrud", 666, "1234657980125"),
+            new Bog("Poter", "Gertrud", 12, "1234657980126")
+        };
 
         // GET: api/Bog
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Bog> Get()
         {
-            return new string[] { "value1", "value2" };
+            return BogList;
         }
 
         // GET: api/Bog/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        [HttpGet("{isbn}", Name = "Get")]
+        public Bog Get(string isbn)
         {
-            return "value";
+            return BogList.Find(bog => bog.Isbn == isbn);
         }
 
         // POST: api/Bog
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Bog value)
         {
+            BogList.Add(value);
         }
 
         // PUT: api/Bog/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut("{isbn}")]
+        public void Put(string isbn, [FromBody] Bog value)
         {
+            Bog bog = Get(isbn);
+            if (bog != null)
+            {
+                bog.Isbn = value.Isbn;
+                bog.Titel = value.Titel;
+                bog.SideTal = value.SideTal;
+                bog.Forfatter = value.Forfatter;
+            }
         }
 
         // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete("{isbn}")]
+        public void Delete(string isbn)
         {
+            Bog bog = Get(isbn);
+            BogList.Remove(bog);
         }
     }
 }
